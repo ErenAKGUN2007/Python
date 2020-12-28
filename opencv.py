acaebe9121d839cc9843448b820e88d66aed5c72
ErenAKGUN2007 <erenakgun2007@hotmail.com>
2020-12-28 22:00:18 +0300

    28.12.2020

---
+++ opencv.py
@@ -0,0 +1,5 @@
+import cv2
+import numpy as np
+image=np.random.randint(0,255,size=(600,600,3),dtype=np.uint8)
+cv2.imshow("Rastgele",image)
+cv2.waitKey(0)
---
+++ oztoplam.py
@@ -0,0 +1,21 @@
+def topla(liste):
+    toplam=0
+    if isinstance(liste,(int,float)):
+        return liste
+    for i in liste:
+        if isinstance(liste,dict):
+            toplam+=topla(liste.get(i))
+        elif isinstance(i,(list,tuple,dict,set)):
+            toplam+=topla(i)
+        elif isinstance(i,(int,float)):
+            toplam+=i
+    return toplam
+if __name__=="__main__":
+    t1=["12","ali",1,2.0,10]
+    t2=(1,2,(10,3,{3,6,3}))
+    t3={"a":1,"b":[t1,t2]}
+    t4=(1,2,[3,(4,5,{7,8}),{"a":12,"b":[3,8,[12]]}])
+    print(topla(t1))
+    print(topla(t2))
+    print(topla(t3))
+    print(topla(t4))
