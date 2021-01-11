540d5cb73365bd868e4dd9421a90d366ff75e007
Eren AKGÜN <erenakgun2007@hotmail.com>
2021-01-11 17:10:13 +0300

    11.01.2021

--- opencv/ResimOkuma.py
+++ opencv/ResimOkuma.py
@@ -1,17 +1,17 @@
-import cv2
+from cv2 import imread, waitKey, imshow, line, rectangle
 import numpy as np
 from os import getcwd, path
 cd=getcwd()
-impath=path.join(getcwd(),"opencv","images","yolResmi.jpg")
-image=cv2.imread(impath)#Tipi nparray, uint8
-cv2.imshow("Yol",image)
-cv2.waitKey(0)
+impath=path.join(getcwd(),"opencv","images","road.jpg")
+image=imread(impath)#Tipi nparray, uint8
+imshow("Yol",image)
+waitKey(0)
 
 
 #1/4 Küçültme
 image1=np.copy(image[::4,::4,:])
-cv2.imshow("Yol Küçük",image1)
-cv2.waitKey(0)
+imshow("Yol Küçük",image1)
+waitKey(0)
 shape=image.shape
 h=shape[0]
 w=shape[1]
@@ -25,8 +25,8 @@ print(ilkrnk)
 #bir grup pikseli değiştirme
 image2=np.copy(image)
 image2[0:100,0:100]=ilkrnk
-cv2.imshow("Yol Boyalı",image2)
-cv2.waitKey(0)
+imshow("Yol Boyalı",image2)
+waitKey(0)
 
 
 #Çizgi
@@ -35,13 +35,19 @@ start=(w//2,h//2)
 end=(w,h)
 renk=(0,0,255)#BGR
 kalinlik=4
-cv2.line(image3,start,end,renk,kalinlik)
-cv2.imshow("Yol Çizgili",image3[::2,::2])#1/2
-cv2.waitKey(0)
+line(image3,start,end,renk,kalinlik)
+imshow("Yol Çizgili",image3[::2,::2])#1/2
+waitKey(0)
 
 
 #Dikdörtgen
 image4=np.copy(image)
-cv2.rectangle(image4,start,end,renk,kalinlik)
-cv2.imshow("Yol Dikgörtgenli",image4[::2,::2])#1/2
-cv2.waitKey(0)
+rectangle(image4,start,end,renk,kalinlik)
+imshow("Yol Dikgörtgenli",image4[::2,::2])#1/2
+waitKey(0)
+
+
+
+"""YENİ RESİM"""""""""""""""""""""""""""""""""""""""""""""""""""
+cd=getcwd()
+impath=path.join(getcwd(),"opencv","images","chp1","marsrover.png")
